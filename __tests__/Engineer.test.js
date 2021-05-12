55d6322aadb605a30c1ad5ba926e4fdd2519f104
Marco <markforester93@gmail.com>
2021-05-13 01:41:21 +1000

    Updated Test Suites

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,20 +3,20 @@ const Engineer = require("../lib/engineer.js");
 
 it("Can set GitHub account via constructor argument", () => {
   const employee = new Engineer(
-    "Andrew",
-    7456,
-    "andrew@gmail.com",
-    "andrew123"
+    "Mark",
+    1,
+    "Mark@example.com",
+    "markexample123"
   );
-  expect(employee.github).toBe("andrew123");
+  expect(employee.github).toBe("markexample123");
 });
 
 it("getRole() should return 'Engineer'", () => {
   const employee = new Engineer(
-    "Andrew",
-    7456,
-    "andrew@gmail.com",
-    "andrew123"
+    "Mark",
+    1,
+    "Mark@example.com",
+    "markexample123"
   );
 
   expect(employee.getRole()).toBe("Engineer");
@@ -24,11 +24,11 @@ it("getRole() should return 'Engineer'", () => {
 
 it("Can get Github username via getGithub()", () => {
   const employee = new Engineer(
-    "Andrew",
-    7456,
-    "andrew@gmail.com",
-    "andrew123"
+    "Mark",
+    1,
+    "Mark@example.com",
+    "markexample123"
   );
 
-  expect(employee.getGithub()).toBe("andrew123");
+  expect(employee.getGithub()).toBe("markexample123");
 });
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,19 +2,19 @@ const Employee = require("../lib/employee.js");
 const Intern = require("../lib/intern.js");
 
 it("Can set school via constructor argument", () => {
-  const employee = new Intern("John", 509, "john@gmail.com", "UTS");
+  const employee = new Intern("Mark", 1, "mark@example.com", "School");
 
-  expect(employee.school).toBe("UTS");
+  expect(employee.school).toBe("School");
 });
 
 it("Can get school via getSchool()", () => {
-  const employee = new Intern("John", 509, "john@gmail.com", "UTS");
+  const employee = new Intern("Mark", 1, "mark@example.com", "School");
 
-  expect(employee.getSchool()).toBe("UTS");
+  expect(employee.getSchool()).toBe("School");
 });
 
 it("getRole() should return 'Intern'", () => {
-  const employee = new Intern("John", 509, "john@gmail.com", "UTS");
+  const employee = new Intern("Mark", 1, "mark@example.com", "School");
 
   expect(employee.getRole()).toBe("Intern");
 });
