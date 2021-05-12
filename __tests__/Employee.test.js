cb69fab33b97195e7596ae549b89d9024906d098
Marco <markforester93@gmail.com>
2021-05-13 00:19:23 +1000

    Updated test suits, updated README.md

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,43 +6,43 @@ it("Can instantiate the Employee Instance", () => {
 });
 
 it("Can set name via constructor arguments", () => {
-  const employee = new Employee("Mary");
+  const employee = new Employee("Mark");
 
-  expect(employee.name).toBe("Mary");
+  expect(employee.name).toBe("Mark");
 });
 
 it("Can set id via constructor argument", () => {
-  const employee = new Employee("Mary", 1033);
+  const employee = new Employee("Mark", 1);
 
-  expect(employee.id).toBe(1033);
+  expect(employee.id).toBe(1);
 });
 
 it("Can set email via constructor argument", () => {
-  const employee = new Employee("Mary", 1033, "mary@gmail.com");
+  const employee = new Employee("Mark", 1, "mark@company.com");
 
-  expect(employee.email).toBe("mary@gmail.com");
+  expect(employee.email).toBe("mark@company.com");
 });
 
 it("Can get name via getName()", () => {
-  const employee = new Employee("Mary", 1033, "mary@gmail.com");
+  const employee = new Employee("Mark", 1, "mark@company.com");
 
-  expect(employee.getName()).toBe("Mary");
+  expect(employee.getName()).toBe("Mark");
 });
 
 it("Can get id via getID()", () => {
-  const employee = new Employee("Mary", 1033, "mary@gmail.com");
+  const employee = new Employee("Mark", 1, "mark@company.com");
 
-  expect(employee.getID()).toBe(1033);
+  expect(employee.getID()).toBe(1);
 });
 
 it("Can get email via getEmail()", () => {
-  const employee = new Employee("Mary", 1033, "mary@gmail.com");
+  const employee = new Employee("Mark", 1, "mark@company.com");
 
-  expect(employee.getEmail()).toBe("mary@gmail.com");
+  expect(employee.getEmail()).toBe("mark@company.com");
 });
 
 it("getRole() should return 'Employee'", () => {
-  const employee = new Employee("Mary", 1033, "mary@gmail.com");
+  const employee = new Employee("Mark", 1, "mark@company.com");
 
   expect(employee.getRole()).toBe("Employee");
 });
